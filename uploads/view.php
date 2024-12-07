d945c284a5be3b1ac9acaea1de209c576425afed
Matt Harrisburg <matthew.harrisburg@gmail.com>
2024-12-07 00:08:11 -0500

    commit

--- uploads/view.php
+++ uploads/view.php
@@ -44,6 +44,7 @@ $conn->close();
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View <?php echo htmlspecialchars($fileName); ?></title>
